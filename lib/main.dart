f5611140086da07eead552c60a169cc28711109c
birehan <91595957+birehan@users.noreply.github.com>
2022-04-12 09:51:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class _StrowState extends State<Strow> {
                     Center(
                       child: Container(
                           width: 400,
-                          padding: const EdgeInsets.all(20),
+                          padding: const EdgeInsets.only(bottom: 20),
                           decoration: const BoxDecoration(
                               image: DecorationImage(
                                   image: AssetImage("assets/image5.jpeg"),
@@ -89,29 +89,81 @@ class _StrowState extends State<Strow> {
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                              Column(
                                 children: [
-                                  const Icon(
-                                    Icons.arrow_back_ios,
-                                    color: Colors.white,
-                                    size: 30,
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8, right: 8, top: 8),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: const [
+                                            Text(
+                                              "Carrier",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w400,
+                                                  fontSize: 18),
+                                            ),
+                                            Icon(
+                                              Icons.wifi,
+                                              color: Colors.white,
+                                              size: 25,
+                                            ),
+                                          ],
+                                        ),
+                                        const Text(
+                                          "7:56 AM",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 18),
+                                        ),
+                                        const RotatedBox(
+                                          quarterTurns: -3,
+                                          child: Icon(
+                                            Icons.battery_full,
+                                            color: Colors.white,
+                                            size: 25,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                  Row(
-                                    children: const [
-                                      Icon(
-                                        Icons.edit,
-                                        color: Colors.white,
-                                        size: 30,
-                                      ),
-                                      Icon(
-                                        Icons.menu,
-                                        color: Colors.white,
-                                        size: 30,
-                                      )
-                                    ],
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 8, left: 12, right: 12),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Icon(
+                                          Icons.arrow_back_ios,
+                                          color: Colors.white,
+                                          size: 25,
+                                        ),
+                                        Row(
+                                          children: const [
+                                            Icon(
+                                              Icons.edit,
+                                              color: Colors.white,
+                                              size: 25,
+                                            ),
+                                            Icon(
+                                              Icons.more_vert,
+                                              color: Colors.white,
+                                              size: 25,
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ],
                               ),
